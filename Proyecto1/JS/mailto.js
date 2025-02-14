637c9bf61e5b17a610817c87051fd56717ce1952
FerVtes25 <120424378+FerVtes25@users.noreply.github.com>
2025-02-14 18:58:40 +0100

    entrega 1

--- Proyecto1/JS/mailto.js
+++ Proyecto1/JS/mailto.js
@@ -1,14 +1,16 @@
-function enviarMail() {
-        var emailDestino = "fvieites@ucm.es";
-        var nombre = document.getElementById("nombre").value;
-        var motivo = document.querySelector('input[name="motivo"]:checked').value;
-        var mensaje = document.getElementById("mensaje").value;
+function enviarMail(event) {
+    event.preventDefault();
 
-        var cuerpoMensaje = "Nombre: " + nombre + "\n\nMensaje:\n" + mensaje;
-    
-        var mailtoLink = "mailto:" + emailDestino +
-                         "?subject=" + encodeURIComponent(motivo) +
-                         "&body=" + encodeURIComponent(cuerpoMensaje);
-    
-        window.location.href = mailtoLink;
+    var emailIngresado = document.getElementById("email").value;
+    var nombre = document.getElementById("nombre").value;
+    var motivo = document.querySelector('input[name="motivo"]:checked').value;
+    var mensaje = document.getElementById("mensaje").value;
+
+    var cuerpoMensaje = "Nombre: " + nombre + "\n\nMensaje:\n" + mensaje;
+
+    var mailtoLink = "mailto:" + emailIngresado +
+                     "?subject=" + encodeURIComponent(motivo) +
+                     "&body=" + encodeURIComponent(cuerpoMensaje);
+
+    window.location.href = mailtoLink;
 }
