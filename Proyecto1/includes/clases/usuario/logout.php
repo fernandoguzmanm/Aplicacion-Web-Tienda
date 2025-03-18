554a6f5fda680506aa60d3a6988e63a204442eb3
FerVtes25 <120424378+FerVtes25@users.noreply.github.com>
2025-03-18 18:43:22 +0100

    falta reorganizar carpetas pero ns muy bine como

--- Proyecto1/includes/clases/usuario/logout.php
+++ Proyecto1/includes/clases/usuario/logout.php
@@ -9,22 +9,22 @@ if (session_status() == PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logout</title>
-    <link rel="stylesheet" href="CSS/estilo.css">
+    <link rel="stylesheet" href="../../../CSS/estilo.css">
 </head>
 <body>
     <div id="contenedor">
-        <?php include("includes/vistas/comun/header.php"); ?>
+        <?php include("../../../includes/vistas/comun/header.php"); ?>
         
         <div id="contenido">
             <?php
             if (isset($_SESSION["login"])) {
                 echo "<h2>Adiós</h2>";
                 echo "<h4>Gracias por visitar nuestra web. Hasta pronto.<h4>";
-                echo "<a href='index.php'>Volver a la página principal</a>";
+                echo "<a href='::7::7::7index.php'>Volver a la página principal</a>";
                 session_destroy();
             } else {
                 echo "<h2>No hay sesión iniciada</h2>";
-                echo "<p><a href='index.php'>Volver a la página principal</a></p>";
+                echo "<p><a href='../../../index.php'>Volver a la página principal</a></p>";
             }
             ?>
         </div>
--- Proyecto1/includes/vistas/comun/header.php
+++ Proyecto1/includes/vistas/comun/header.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="./css/cssHeader.css?v=2">
-</head>
+
 <body>
 
 <?php
@@ -14,21 +11,26 @@ $page_title = basename($_SERVER['PHP_SELF'], ".php");
 
 // Contar productos en el carrito
 $carrito_count = isset($_SESSION['carrito']) ? array_sum(array_column($_SESSION['carrito'], 'cantidad')) : 0;
-
+$base_url = "/AW/Proyecto1/";
 ?>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="<?=$base_url ?>css/cssHeader.css?v=2">
+</head>
 
 <header>
     <div class="header-container">
         <h1> <?php echo ucfirst($page_title); ?> </h1>
         <nav>
             <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="tienda.php">Tienda</a></li>
-                <li><a href="detalles.php">Detalles</a></li>
-                <li><a href="bocetos.php">Bocetos</a></li>
-                <li><a href="miembros.php">Miembros</a></li>
-                <li><a href="planificacion.php">Planificación</a></li>
-                <li><a href="contacto.php">Contacto</a></li>
+                <li><a href="<?=$base_url ?>index.php">Inicio</a></li>
+                <li><a href="<?=$base_url ?>tienda.php">Tienda</a></li>
+                <li><a href="<?=$base_url ?>detalles.php">Detalles</a></li>
+                <li><a href="<?=$base_url ?>bocetos.php">Bocetos</a></li>
+                <li><a href="<?=$base_url ?>miembros.php">Miembros</a></li>
+                <li><a href="<?=$base_url ?>planificacion.php">Planificación</a></li>
+                <li><a href="<?=$base_url ?>contacto.php">Contacto</a></li>
             </ul>
         </nav>
         
@@ -36,9 +38,9 @@ $carrito_count = isset($_SESSION['carrito']) ? array_sum(array_column($_SESSION[
             <div id="login-container">
                 <?php if (isset($_SESSION['usuario'])) : ?>
                     <p>Bienvenido, <?php echo htmlspecialchars($_SESSION['nombre']); ?></p>
-                    <p><a href="logout.php">Cerrar sesión</a></p>
+                    <p><a href="includes/clases/usuario/logout.php">Cerrar sesión</a></p>
                 <?php else : ?>
-                    <p><a href="login.php">Iniciar sesión</a></p>
+                    <p><a href="includes/clases/usuario/login.php">Iniciar sesión</a></p>
                 <?php endif; ?>
             </div>
 
